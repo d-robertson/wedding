cb30fcb55934a8bcb39273156ba1739336208ada
Sean Robertson <srobertson421@gmail.com>
2018-06-22 15:35:08 -0700

    Added twitch stream to stream page

--- weddingApp/src/components/aStream.vue
+++ weddingApp/src/components/aStream.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="stream">
-    <h1>STream</h1>
+    <h1>Stream</h1>
+    <iframe src="https://player.twitch.tv/?channel=largemoose421" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/largemoose421?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from largemoose421 on www.twitch.tv</a>
+    <iframe src="https://www.twitch.tv/embed/largemoose421/chat" frameborder="0" scrolling="no" height="500" width="350"></iframe>
   </div>
 </template>
 
@@ -33,6 +35,6 @@ a {
 }
 
 div.stream{
- 
+
 }
 </style>
